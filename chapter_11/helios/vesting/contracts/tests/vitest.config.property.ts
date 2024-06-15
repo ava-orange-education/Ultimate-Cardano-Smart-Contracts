c8a8d010c808f94e8d3dda8a120c51ea0287d065
AVA™ <102225901+OrangeAVA@users.noreply.github.com>
2024-06-15 11:39:52 +0530

    Delete chapter_11 directory

--- chapter_11/helios/vesting/contracts/tests/vitest.config.property.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  test: {
-    include: ['contracts/tests/*.property.test.ts'],
-    exclude: ['contracts/tests/*.config.*'],
-  }
-});
--- chapter_11/helios/vesting/contracts/tests/vitest.config.test.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  test: {
-    include: ['contracts/tests/*.test.ts'],
-    exclude: ['contracts/tests/*.property.test.*',
-              'contracts/tests/*.config.*']
-  }
-});
